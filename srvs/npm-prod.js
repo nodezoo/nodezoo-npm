76f2bb99735ac39d4f16126222b957a2470e7a1e
Richard Rodger <richard@ricebridge.com>
2014-09-04 02:11:13 +0100

    srvs

---
+++ srvs/npm-prod.js
@@ -0,0 +1,25 @@
+
+var seneca = require('seneca')()
+
+      .use('redis-transport')
+      .use('jsonfile-store',{folder:__dirname+'/../data'})
+      .use('../npm.js')
+
+      .add('role:info,req:part',function(args,done){
+        done()
+
+        this.act(
+          'role:npm,cmd:get',
+          {name:args.name},
+          function(err,mod){
+            if( err ) return done(err);
+
+            this.act('role:info,res:part,part:npm',
+                     {name:args.name,data:mod.data$()})
+          })
+      })
+
+      .listen({type:'redis',pin:'role:info,req:part'})
+      .client({type:'redis',pin:'role:info,res:part'})
+
+      .listen(9001)

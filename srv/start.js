7e067b8393d5e36bd83da57dabb63842b0faaf48
Shane Lacey <shane.lacey@nearform.com>
2016-04-05 16:49:15 +0100

    Linted

--- srv/start.js
+++ srv/start.js
@@ -1,24 +1,20 @@
-
-var HOST = process.env.HOST || 'localhost'
-var STATS = process.env.STATS || 'localhost'
-
 require('seneca')()
   .use('entity')
   .use('../lib/npm.js')
-  .add('role:info,req:part',function (args,done) {
+  .add('role:info,req:part', function (args, done) {
     done()
 
-    this.act('role:npm,cmd:get', {name:args.name},function (err, mod) {
+    this.act('role:npm,cmd:get', {name: args.name}, function (err, mod) {
       if (err) {
-        return done(err);
+        return done(err)
       }
 
-        this.act('role:info,res:part,part:npm', {name:args.name,data:mod.data$()})
-      })
+      this.act('role:info,res:part,part:npm', {name: args.name, data: mod.data$()})
+    })
   })
 
-  .add('role:npm,info:change', function (args,done) {
+  .add('role:npm,info:change', function (args, done) {
     done()
-    this.act('role:npm,cmd:get', {name:args.name,update:true})
+    this.act('role:npm,cmd:get', {name: args.name, update: true})
   })
-  .use('mesh', {auto:true, pins:['role:npm','role:info,req:part'], model:'publish'} )
+  .use('mesh', {auto: true, pins: ['role:npm', 'role:info,req:part'], model: 'publish'})
